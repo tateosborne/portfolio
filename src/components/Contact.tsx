8a9f334417c51d97f99f9acf3da19e1b4cefa956
Tate P. Osborne <tate0301@gmail.com>
2023-07-29 18:11:27 -0400

    contact page progress

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,6 +8,13 @@ function Contact() {
             <div className="content-container">
                 <h2>REACH OUT! 📬</h2>
                 <h3>don't hesitate to contact me or checkout my socials</h3>
+                <div className="email-container">
+                    <p
+                        dangerouslySetInnerHTML={{__html: svgs.email}}>
+                    </p>
+                    <p>tateposborne@icloud.com</p>
+
+                </div>
             </div>
         </div>
     )
