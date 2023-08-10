ed1214f32a9c531869fb7a79c047125fc14d86c5
Tate P. Osborne <tate0301@gmail.com>
2023-08-10 12:07:37 -0400

    organizing the artwork container

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -5,12 +5,17 @@ function Artwork() {
 
     return (
         <div className="artwork-container" id="artwork">
-            <h2>ARTWORK 🎨</h2>
-            <h3>check out some of my artwork!</h3>
-            <div className="allessio">
-                <video controls poster="../thumbnail.png">
-                    <source src="../artwork/allessio.mp4" type="video/mp4" />
-                </video>
+            <div className="header-container">
+                <h2>ARTWORK 🎨</h2>
+                <h3>check out some of my artwork!</h3>
+            </div>
+            <div className="content-container">
+                <div className="allessio">
+                    <video controls poster="../thumbnail.png">
+                        <source src="../artwork/allessio.mp4" type="video/mp4" />
+                    </video>
+                    <p>summary of allessio</p>
+                </div>
             </div>
         </div>
     )
