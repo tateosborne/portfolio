a2b5d5b1134d1335a10d5b09fcae6bc39e4349df
Tate P. Osborne <tate0301@gmail.com>
2023-08-10 11:20:39 -0400

    rebasing project; unexpected slow pushed occurring

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -7,6 +7,11 @@ function Artwork() {
         <div className="artwork-container" id="artwork">
             <h2>ARTWORK 🎨</h2>
             <h3>check out some of my artwork!</h3>
+            <div className="allessio">
+                <video controls poster="../thumbnail.png">
+                    {/* <source src="../artwork/allessio.mp4" type="video/mp4" /> */}
+                </video>
+            </div>
         </div>
     )
 }
