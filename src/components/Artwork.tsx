20d84fb4da9ec37d54d38e889df9c1136a87b1df
Tate P. Osborne <tate0301@gmail.com>
2023-08-12 00:05:22 -0400

    first art tile completed

--- src/components/Artwork.tsx
+++ src/components/Artwork.tsx
@@ -10,11 +10,16 @@ function Artwork() {
                 <h3>check out some of my artwork!</h3>
             </div>
             <div className="content-container">
-                <div className="allessio">
-                    <video controls poster="../thumbnail.png">
-                        <source src="../artwork/allessio.mp4" type="video/mp4" />
-                    </video>
-                    <p>summary of allessio</p>
+                <div className="each-art">
+                    <div className="blurb">
+                        <h4>Allessio's Life</h4>
+                        <p>I created a meticulously designed an animated film that captures a couple days of a fictional chef. I used a camera to capture each frame of the animation. I hand drew each image and cut them out, before meticulously arranging these cutouts to portray the given scene. This project required a dedicated and consistent two-month effort to bring to completion.</p>
+                    </div>
+                    <div className="artwork">
+                        <video controls poster="../thumbnail.png">
+                            <source src="../artwork/allessio.mp4" type="video/mp4" />
+                        </video>
+                    </div>
                 </div>
             </div>
         </div>
