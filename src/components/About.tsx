3a26a5cc14d2fa146ce1738127e2539ee8452be6
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2024-10-23 23:36:47 -0400

    age updates dynamically on my birthday

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -15,6 +15,17 @@ const About: React.FC<Props> = ({theme, sideBar, setSideBar}) => {
        blur = "blur";
     }
 
+    const today = new Date();
+  
+    let age = today.getFullYear() - 2001;
+    
+    const monthDiff = today.getMonth() - 3;
+    const dayDiff = today.getDate() - 12
+    
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+
     return (
         <div className={theme}>
             <div className={blur}>
@@ -26,7 +37,8 @@ const About: React.FC<Props> = ({theme, sideBar, setSideBar}) => {
                     <div className="content-container">
                         <img src="../../emojis/memoji-coding.png" alt="coding cartoon" />
                         <div className="body-text">
-                            <p>Hey! I am an England-born 22 year old developer. I graduated from the <span>University of Vermont</span> in 2023 with a Bachelors of Science in <span>Computer Science</span> and an <span>Art Studio</span> Minor.</p>
+                            <p>Hey! I am an England-born {age} year-old developer. I graduated from the <span>University of Vermont</span> in 2023 with a Bachelors of Science in <span>Computer Science</span> and an <span>Art Studio</span> Minor.</p>
+                            <p>I am currently purusing my Master's of Science in <span>Computer Science</span> at <span>Georgia Tech</span>, expecting to graduate in the spring of 2026.</p>
                             <p>I have hobbies in soccer ⚽️, travelling ✈️, creating art 🎨, cooking 🧑🏽‍🍳, and speed-solving Rubik's cubes <span dangerouslySetInnerHTML={{__html: svgs.rubikscube}}></span></p>
                             <p>In my work, I obsess over <span>user experience</span> and <span>logical</span> structure.</p>
                         </div>
