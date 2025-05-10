06e588fbef923de3ce118acebf199137af8a6ac5
Tate Osborne <89999710+tateosborne@users.noreply.github.com>
2025-05-09 21:25:09 -0400

    handler logic with endpoints

--- main.go
+++ main.go
@@ -9,19 +9,31 @@ import (
 
 type welcome string
 
+// handler interface
 func (wc welcome) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome to my server!")
 }
 
+// handler function
+func gallery(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Photo gallery!")
+}
+
 func main() {
+	// router
+	router := http.NewServeMux()
 
 	// handler
 	var wc welcome
+	router.Handle("/", wc)
+
+	// handler funcs
+	router.HandleFunc("/gallery", gallery)
 
 	// server
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: wc,
+		Handler: router,
 	}
 
 	// run server
